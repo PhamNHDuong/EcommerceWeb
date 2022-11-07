f56316831bf64f99d4ee5e4028361bf0f8832d32
catb0t0 <66787728+catb0t0@users.noreply.github.com>
2022-11-07 18:05:19 +0700

    Mapping

---
+++ EcommerceWeb.API/Utilities/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EcommerceWeb.Data.Entities;
+using EcommerceWeb.Dto.Models;
+
+namespace EcommerceWeb.API.Utilities
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDto>();
+
+            CreateMap<Category, CategoryDto>();
+
+            CreateMap<Rating, RatingDto>();
+        }
+    }
+}
